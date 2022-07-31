d6fab82bcb6435d07b3331b988d71334cf97f7aa
esmaelgh23-meet <109899626+esmaelgh23-meet@users.noreply.github.com>
2022-07-31 16:43:40 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAR1N-fr17lyOgRHaPZZBK68_fURX-Eh_4",
+  authDomain: "esmael-e2636.firebaseapp.com",
+  projectId: "esmael-e2636",
+  storageBucket: "esmael-e2636.appspot.com",
+  messagingSenderId: "259011972273",
+  appId: "1:259011972273:web:b28ef260699a00c2132690",
+  measurementId: "G-1WYXG9QVRD"
+};
+
+firebase = pyrebase.intitialize_app(firebaseConfig)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
@@ -23,3 +37,32 @@ def add_tweet():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
